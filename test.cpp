f52f81b1972ee47d86a29b5a0a09b2f519ed7221
trongtn2110@gmail.com <trongtn2110@gmail.com>
2020-09-27 14:00:01 +0700

    hi

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// namespace std;
+
+int main()
+{
+    int a =5;
+    int b = 6;
+    cout<<"Nhap mot so\n";
+    int c;
+    cin>>c;
+    cout<<"\nhello\n";
+    cout<<a+b+c;
+    return 0;
+}
+
